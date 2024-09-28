25a71fc259dd23075bd560382b95610c8d76f110
dev-rajnish <dev.rajnish@proton.me>
2024-09-28 23:18:30 +0530

    final

--- public/script.js
+++ public/script.js
@@ -6,8 +6,16 @@ window.onload = async function () {
 
   // Extract the short code from the URL
   const hashShortCode = window.location.hash.substring(1);
-
-  if (hashShortCode) {
+  let link = window.location.href
+  const fragmentLink = link.split('/')
+  console.log('11', fragmentLink)
+  const l = fragmentLink.pop()
+  console.log('13',l)
+  console.log('14',hashShortCode)
+  let sCode = hashShortCode==='' ? l : hashShortCode
+console.log('16',sCode);
+  if (sCode) {
+    console.log('18',sCode)
       // Fetch the data from Google Sheets
       const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/url!A:B?key=${API_KEY}`;
       console.log('Fetching data from:', url);
@@ -18,23 +26,28 @@ window.onload = async function () {
           // Loop through the sheet data to find a matching short code
           let found = false;
           data.values.forEach(row => {
-              if (row[0] === hashShortCode) {
+              if (row[0] === sCode) {
+                console.log('30',sCode)
                   window.location.href = row[1]; // Redirect to the original URL
+                  console.log(row[1])
                   found = true;
               }
           });
 
           if (!found) {
               document.body.innerHTML = "<p>Shortened link not found.</p>";
+              window.location.href = 'https://rsh.web.app/#lsurl'
               document.body.style.display = 'block'; // Show body content if not found
           }
       } catch (error) {
           console.error("Error fetching link: ", error);
           document.body.innerHTML = "<p>Error occurred while fetching the link.</p>";
+          window.location.href = 'https://rsh.web.app/#lsurl'
           document.body.style.display = 'block'; // Show body content on error
       }
   } else {
       document.body.innerHTML = "<p>No short code provided in the URL.</p>";
+      window.location.href = 'https://rsh.web.app/#lsurl'
       document.body.style.display = 'block'; // Show body content if no short code
   }
 };
